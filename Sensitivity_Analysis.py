3d469b3bd66b615937ff575644d317ab8bc3a4d3
MerlijnBroekers2 <merlijnbroekers@icloud.com>
2024-05-21 12:53:26 +0200

    Sensitivity finished

--- Sensitivity_Analysis.py
+++ Sensitivity_Analysis.py
@@ -76,7 +76,7 @@ def sensitivity_analysis_delay(n_targets, n_drones, endurance, max_delay):
 def sensitivity_analysis_endurance(n_targets, max_drones, max_endurance, delay=1, num_matrices=5, max_time=30):
     results = []
     for n_drones in range(1, max_drones + 1):
-        for endurance in range(10, max_endurance + 1, 1):
+        for endurance in range(10, max_endurance + 1, 5):
             obj_vals = []
             elapsed_times = []
             for _ in range(num_matrices):
@@ -104,7 +104,7 @@ def sensitivity_analysis_endurance(n_targets, max_drones, max_endurance, delay=1
 def combined_sensitivity_analysis(max_targets, max_drones, endurance, delay, num_matrices=5, max_time=30):
     results = []
     for n_targets in range(1, max_targets + 1):
-        for n_drones in range(n_targets, max_drones + 1):
+        for n_drones in range(n_targets + 1, max_drones + 1):
             for _ in range(num_matrices):
                 time_matrix = create_random_time_matrix(n_targets, n_drones, max_time)
                 model = UAVStrikeModel(n_targets=n_targets, n_uavs=n_drones, endurance=endurance, delay=delay)
@@ -179,37 +179,37 @@ def plot_heatmap_endurance_drones(csv_file):
 # Example usage
 if __name__ == "__main__":
     # Parameters
-    max_targets = 4
+    max_targets = 3
     max_drones = 10
-    max_endurance = 40
+    max_endurance = 100
     delay = 5
-    num_matrices = 20
+    num_matrices = 10
 
     # Combined sensitivity analysis
-    combined_results = combined_sensitivity_analysis(max_targets=max_targets, max_drones=max_drones, endurance=max_endurance, delay=delay, num_matrices=num_matrices)
+    # combined_results = combined_sensitivity_analysis(max_targets=max_targets, max_drones=max_drones, endurance=max_endurance, delay=delay, num_matrices=num_matrices)
 
     # Sensitivity analysis for endurance
-    # endurance_results = sensitivity_analysis_endurance(n_targets=max_targets, max_drones=max_drones, max_endurance=max_endurance, delay=delay, num_matrices=num_matrices)
+    endurance_results = sensitivity_analysis_endurance(n_targets=max_targets, max_drones=max_drones, max_endurance=max_endurance, delay=delay, num_matrices=num_matrices)
 
     # Construct the filename with parameters
-    csv_file_combined = f'Sensitivity_data/sensitivity_analysis_results_max_targets_{max_targets}_max_drones_{max_drones}_endurance_{max_endurance}_delay_{delay}_num_matrices_{num_matrices}.csv'
-    # csv_file_endurance = f'Sensitivity_data/sensitivity_analysis_endurance_max_targets_{max_targets}_max_drones_{max_drones}_max_endurance_{max_endurance}_delay_{delay}_num_matrices_{num_matrices}.csv'
+    # csv_file_combined = f'Sensitivity_data/sensitivity_analysis_results_max_targets_{max_targets}_max_drones_{max_drones}_endurance_{max_endurance}_delay_{delay}_num_matrices_{num_matrices}.csv'
+    csv_file_endurance = f'Sensitivity_data/sensitivity_analysis_endurance_max_targets_{max_targets}_max_drones_{max_drones}_max_endurance_{max_endurance}_delay_{delay}_num_matrices_{num_matrices}.csv'
 
-    # Save results to CSV
-    # save_results_to_csv(combined_results, csv_file_combined)
+    # # Save results to CSV
+    # # save_results_to_csv(combined_results, csv_file_combined)
     # save_results_to_csv(endurance_results, csv_file_endurance)
-
-    # Heatmap for Targets vs Drones
-    plot_heatmap_targets_drones(csv_file_combined)
-
-    # Boxplot for Time Matrix
-    plot_boxplot_time_matrix(csv_file_combined)
-
-    # Line Plot for Delays
-    plot_line_delay(csv_file_combined)
-
-    # Scatter Plot for Elapsed Time vs Objective Value
-    plot_scatter_elapsed_time(csv_file_combined)
+    #
+    # # Heatmap for Targets vs Drones
+    # plot_heatmap_targets_drones(csv_file_combined)
+    #
+    # # Boxplot for Time Matrix
+    # plot_boxplot_time_matrix(csv_file_combined)
+    #
+    # # Line Plot for Delays
+    # plot_line_delay(csv_file_combined)
+    #
+    # # Scatter Plot for Elapsed Time vs Objective Value
+    # plot_scatter_elapsed_time(csv_file_combined)
 
     # Heatmap for Endurance vs Number of Drones
-    # plot_heatmap_endurance_drones(csv_file_endurance)
+    plot_heatmap_endurance_drones(csv_file_endurance)
